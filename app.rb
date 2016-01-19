e6163f3a882479a24e53f73c104ad3b9e81882ee
Alexander Zhadnov <masteralred@gmail.com>
2016-01-19 11:11:06 +0300

    Restore to AR simple

--- app.rb
+++ app.rb
@@ -12,12 +12,32 @@ class Post < ActiveRecord::Base
 end
 
 class Comment <ActiveRecord::Base
+<<<<<<< HEAD
 	belongs_to :post
 	validates :content, presence: true
+=======
+>>>>>>> parent of f8f9ec8... Fully transfer simple-blog to Active Record
 end
 
+# def fields_validation hh
+# 	hh.each_key do |i|
+# 		if params[i]==''
+# 			@error = "Введите: " + hh.select {|k,_| params[k] == ''}.values.join(", ")
+# 		end
+# 	end
+# end
+
+# before '/details/:post_id' do
+# 	post_id=params[:post_id]
+# 	#db = init_db
+# 	results = db.execute 'SELECT * FROM Posts WHERE Id=?', [post_id]
+# 	@row=results[0]
+# 	@comments = db.execute 'SELECT * FROM Comments WHERE Post_id=? ORDER BY Id', [post_id]
+# 	db.close
+# end
+
 get '/' do
-	@posts = Post.order('created_at DESC')
+	@posts = Post.all
   	erb :index
 end
 
@@ -26,6 +46,13 @@ get '/new' do
 	erb :new
 end
 
+<<<<<<< HEAD
+=======
+get '/details/:post_id' do
+	erb :details
+end
+
+>>>>>>> parent of f8f9ec8... Fully transfer simple-blog to Active Record
 post '/new' do
 	@p = Post.new params[:post]
 	if @p.save
@@ -36,6 +63,7 @@ post '/new' do
 	end
 end
 
+<<<<<<< HEAD
 before '/details/:id' do
 	@post = Post.find(params[:id])
 	#@comments = Comment.where("post_id = ?", [params[:id]])
@@ -56,3 +84,14 @@ post '/details/:id' do
 		erb :details
 	end
 end
+=======
+# post '/details/:post_id' do
+# 	comment=params[:content]
+# 	post_id=params[:post_id]
+# 	(@error="Сначала введите комментарий!"; return erb :details) if comment.strip.empty?
+# 	db = init_db
+# 	db.execute 'INSERT INTO Comments (Post_id, Creation_date, Content) VALUES (?, datetime(), ?)', [post_id, comment]
+# 	db.close
+# 	redirect ('/details/' + post_id)
+# end
+>>>>>>> parent of f8f9ec8... Fully transfer simple-blog to Active Record
